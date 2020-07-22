8aeec1310cd580ae4561b81af1539b1faf7f3487
Shaojia Zhao <shaojiazhao@gmail.com>
2020-07-21 20:24:02 -0700

    Initial Xcode Commit

---
+++ SafariTabManager/SafariTabManager/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SafariTabManager
+//
+//  Created by Shaojia Zhao on 7/21/20.
+//  Copyright © 2020 Shaojia Zhao. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
